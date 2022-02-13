70e33f6c1c2047e09301bbb70fd5479caea7c479
Alex Sexwale <alex.sexwale@nearix.com>
2022-02-13 16:05:25 +0200

    created vue frontend

---
+++ vue-js-client-crud/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <router-link :to="{ name: 'tutorials' }" class="navbar-brand">Young Grasshopper</router-link>
+    <div class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <router-link :to="{ name: 'tutorials-details', params: { id: 1 }}" class="nav-link">Tutorials</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'add'}" class="nav-link">Add</router-link>
+      </li>
+    </div>
+  </nav>
+</template>
---
+++ vue-js-client-crud/src/services/TutorialDataService.js
@@ -0,0 +1,25 @@
+import api from "../api";
+class TutorialDataService {
+  getAll() {
+    return api.get("/tutorials");
+  }
+  get(id) {
+    return api.get(`/tutorials/${id}`);
+  }
+  create(data) {
+    return api.post("/tutorials", data);
+  }
+  update(id, data) {
+    return api.put(`/tutorials/${id}`, data);
+  }
+  delete(id) {
+    return api.delete(`/tutorials/${id}`);
+  }
+  deleteAll() {
+    return api.delete(`/tutorials`);
+  }
+  findByTitle(title) {
+    return api.get(`/tutorials?title=${title}`);
+  }
+}
+export default new TutorialDataService();
